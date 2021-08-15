e5250f117b09c7e0541dd3c79bbe0635cb400e2a
AngiMon <angelina.schmidlin@hotmail.fr>
2021-08-15 14:23:50 +0200

    template integration

--- Makefile
+++ Makefile
@@ -2,11 +2,22 @@ ENCORE=docker-compose run --rm encore
 COMPOSER=docker-compose run --rm web
 CONSOLE=docker-compose run --rm web bin/console
 
+dc_up:
+	docker-compose up -d
+dc_down:
+	docker-compose down
+
+assets_install:
+	${CONSOLE} assets:install
 
 yarn_watch:
 	${ENCORE} yarn dev --watch
 yarn_install:
 	${ENCORE} yarn install
+yarn_add:
+	${ENCORE} yarn add $(cmd)
+yarn_remove:
+	${ENCORE} yarn remove $(cmd)
 composer_install:
 	${COMPOSER} composer install
 composer_require:
--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,29 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
+// // start the Stimulus application
+// import './bootstrap';
+
+// import './bootstrap/css/bootstrap.min.css';
+// import './font-awesome/css/fontawesome-all.min.css';
+// import './css/magnific-popup.css';
+// import './css/owl.carousel.min.css';
+// import './css/style.css';
+
+
+// import './js/jquery-3.3.1.min.js';
+// import './js/popper.min.js';
+
+
+// import './js/custom.hero.js';
+// import './bootstrap/js/bootstrap.min.js';
+// import './js/imagesloaded.pkgd.min.js';
+// import './js/isInViewport.jquery.js';
+// import './js/jquery.magnific-popup.min.js';
+// import './js/owl.carousel.min.js';
+// import './js/scrolla.jquery.min.js';
+// import './js/jquery.validate.min.js';
+// import './js/jquery-validate.bootstrap-tooltip.min.js';
+// import './js/custom.js';
